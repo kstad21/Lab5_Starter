fa23bb96aa368d611c0d6b905fdbf0e60051cbb4
Kate Stadler <khs-git@stadfam.net>
2025-05-06 13:18:40 -0700

    load list of voices working

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,30 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
-}
+  const synth = window.speechSynthesis;
+  const voiceSelect = document.getElementById("voice-select");
+
+  populateVoiceList(voiceSelect, synth);
+
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = () => populateVoiceList(voiceSelect, synth);
+  }
+};
+
+function populateVoiceList(voiceSelect, synth) {
+  const voices = synth.getVoices();
+  voiceSelect.innerHTML = "";
+
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement("option");
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+    if (voices[i].default) {
+      option.textContent += " - DEFAULT";
+    }
+
+    option.setAttribute("data-lang", voices[i].lang);
+    option.setAttribute("data-name", voices[i].name);
+    voiceSelect.appendChild(option);
+  }
+};
