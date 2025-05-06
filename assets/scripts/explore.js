df3997937edf140dd5f39baffe041c4b98105b20
Kate Stadler <khs-git@stadfam.net>
2025-05-06 15:13:25 -0700

    get text to speak working

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -6,12 +6,23 @@ function init() {
   // TODO
   const synth = window.speechSynthesis;
   const voiceSelect = document.getElementById("voice-select");
+  const speakButton = document.querySelector("#explore button");
+  const textInput = document.getElementById("text-to-speak");
 
   populateVoiceList(voiceSelect, synth);
 
   if (speechSynthesis.onvoiceschanged !== undefined) {
     speechSynthesis.onvoiceschanged = () => populateVoiceList(voiceSelect, synth);
   }
+
+  speakButton.addEventListener("click", () => {
+    const toSay = new SpeechSynthesisUtterance(textInput.value);
+    const selectedVoice = voiceSelect.selectedOptions[0].getAttribute("data-name");
+    const voices = synth.getVoices();
+
+    toSay.voice = voices.find(voice => voice.name === selectedVoice);
+    synth.speak(toSay);
+  });
 };
 
 function populateVoiceList(voiceSelect, synth) {
